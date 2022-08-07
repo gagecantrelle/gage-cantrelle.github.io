84207ff378c9d34ecaf9b4ddeb89ac4c1c796cf1
gagecantrelle <gagecantrelle1899@gmail.com>
2022-08-07 19:43:00 +0000

    fghj

--- studies/variables.js
+++ studies/variables.js
@@ -1,9 +1,13 @@
 /**
  * Varaibales
  * 
- * 0: Variables are name containers for data in javascript. they can hold...
+ * 0: Variables are name containers for data/Values in javascript. They can hold diferent types of values like Strings, Numbers, Boolean, Array, Object, and more. 
+ * Once when a variable is created it can change it value or value type at any time. 
  * 
- * 
- * 
- * 
+ * 1: To create a variable type var, follwode by a name or id
+ *   
+ * 2: There are two steps for creating a variable
  */
+
+//step 1. declaraing a name
+//after declaraing a name that varaibale 
